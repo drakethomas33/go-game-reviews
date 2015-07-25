2c71547fedca91d3586ac4a5f8d219016017f8b4
Tom <Thomas@oceanlabs.co>
2015-07-24 18:11:30 -0700

    lets try

--- project/settings/production.py
+++ project/settings/production.py
@@ -39,7 +39,7 @@ AWS_QUERYSTRING_AUTH = False  # For now lets not do this
 AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'go-game-review'
-AWS_PRELOAD_METADATA = False  # necessary to fix manage.py collectstatic command to only upload changed files instead of all files
+AWS_PRELOAD_METADATA = True  # necessary to fix manage.py collectstatic command to only upload changed files instead of all files
 STATIC_URL = ('https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME) # TODO: annoyingly collectstatic fails on EU buckets, only US standard works. Solution?!
 ADMIN_MEDIA_PREFIX = ('https://s3.amazonaws.com/%s/admin' % AWS_STORAGE_BUCKET_NAME)
 
--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,4 +1,3 @@
 $('document').ready(function(){
     mixpanel.track('Landing page view');
-    
 });
